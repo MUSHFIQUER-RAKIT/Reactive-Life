03b341902adfa931cb065c68c61fc12d14f07a08
MUSHFIQUER-RAKIT <mushfiquerrakit@gmail.com>
2024-10-30 09:54:21 +0600

    some

---
+++ src/PutingItTogether/Together.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Playground from "./PlayGround";
+
+export default function Together() {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button onClick={() => setShow(!show)}>
+        {show ? "Unmount" : "Mount"} the component
+      </button>
+      {show && <hr />}
+      {show && <Playground />}
+    </>
+  );
+}
