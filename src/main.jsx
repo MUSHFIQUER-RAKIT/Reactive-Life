095f09a552a44a91c1f0239f9ab2d4eb54b8259c
MUSHFIQUER-RAKIT <mushfiquerrakit@gmail.com>
2024-11-09 23:14:54 +0600

    Client Side Routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Contact from "./Contact";
+import ErrorPage from "./Error";
 import "./index.css";
+import Root from "./Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
   {
     path: "/about",
